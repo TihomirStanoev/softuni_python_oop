97fcf192e50be73b20bf10773b87714f1c4eb868
tihomir <tstanoev@yandex.com>
2025-07-19 14:14:49 +0300

    08. Library

--- 21_classes_and_objects_exercise/8_library/project/library.py
+++ 21_classes_and_objects_exercise/8_library/project/library.py
@@ -18,18 +18,25 @@ class Library:
 
 
     def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
-        rented_days = self._is_rented(book_name)
-
-        if rented_days >= 0:
-            return f'The book "{book_name}" is already rented and will be available in {rented_days} days!'
         if not self._is_available(author, book_name):
-            return None
+            rented_days = self._is_rented(book_name)
+            if rented_days >= 0:
+                return f'The book "{book_name}" is already rented and will be available in {rented_days} days!'
+            return ''
+
         if user.username not in self.rented_books:
             self.rented_books[user.username] = {}
-
         self.rented_books[user.username][book_name] = days_to_return
         user.books.append(book_name)
         self.books_available[author].remove(book_name)
 
         return f'{book_name} successfully rented for the next {days_to_return} days!'
 
+
+    def return_book(self, author:str, book_name:str, user:User):
+        if book_name not in user.books:
+            return f'{user.username} doesn\'t have this book in his/her records!'
+
+        self.books_available[author].append(book_name)
+        del self.rented_books[user.username][book_name]
+        user.books.remove(book_name)
--- 21_classes_and_objects_exercise/8_library/project/registration.py
+++ 21_classes_and_objects_exercise/8_library/project/registration.py
@@ -1,2 +1,32 @@
+from project.user import User
+from project.library import Library
+
 class Registration:
-    pass
+    @staticmethod
+    def _is_registered(user, library):
+        return next((u for u in library.user_records if u.user_id == user.user_id),None)
+
+    @staticmethod
+    def add_user(user:User, library: Library):
+        if not Registration._is_registered(user, library):
+            library.user_records.append(user)
+            return None
+        return f'User with id = {user.user_id} already registered in the library!'
+
+    @staticmethod
+    def remove_user(user:User, library:Library):
+        if not Registration._is_registered(user, library):
+            return 'We could not find such user to remove!'
+        library.user_records.remove(user)
+
+    @staticmethod
+    def change_username(user_id: int, new_username: str, library: Library):
+        user = next((u for u in library.user_records if u.user_id == user_id), None)
+
+        if not user:
+            return f'There is no user with id = {user_id}!'
+        if user.username == new_username:
+            return 'Please check again the provided username - it should be different than the username used so far!'
+
+        user.username = new_username
+        return f'Username successfully changed to: {new_username} for user id: {user_id}'
