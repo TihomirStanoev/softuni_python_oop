ad9fdc58bca1fc9b3b3121f191265470f3212f30
tihomir <tstanoev@yandex.com>
2025-07-22 19:33:10 +0300

    01. Logged

---
+++ 91_decorators_exercise/1_logged.py
@@ -0,0 +1,11 @@
+def logged(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return f'you called {func.__name__}({", ".join(str(el) for el in args)})\nit returned {result}'
+    return wrapper
+
+
+@logged
+def func(*args):
+    return 3 + len(args)
+print(func(4, 4, 4))
