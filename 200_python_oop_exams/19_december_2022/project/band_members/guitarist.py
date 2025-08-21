e97d0bc2e1d1ff67f3aabd3a17184659cb28761c
tihomir <tstanoev@yandex.com>
2025-08-21 22:52:32 +0300

    `12`

---
+++ 200_python_oop_exams/19_december_2022/project/band_members/guitarist.py
@@ -0,0 +1,9 @@
+from project.band_members.musician import Musician
+
+
+class Guitarist(Musician):
+    @property
+    def available_skills(self):
+        return ['play metal',
+                'play rock',
+                'play jazz']
