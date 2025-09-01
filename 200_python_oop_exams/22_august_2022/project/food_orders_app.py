a2d2be79f0a89880acdfa941aac3d013396012f1
tihomir <tstanoev@yandex.com>
2025-09-01 21:02:22 +0300

    `14`

--- 200_python_oop_exams/22_august_2022/project/food_orders_app.py
+++ 200_python_oop_exams/22_august_2022/project/food_orders_app.py
@@ -1,14 +1,21 @@
+from collections import defaultdict
+
 from project.meals.meal import Meal
 from project.client import Client
 
 class FoodOrdersApp:
     VALID_MEALS = {'Starter', 'MainDish', 'Dessert'}
+    MIN_MEALS = 5
     def __init__(self):
         self.menu: list[Meal]= []
         self.clients_list: list[Client] = []
 
     def _find_client_by_number(self,client_phone_number ):
         return next((c for c in self.clients_list if c.phone_number == client_phone_number), None)
+
+    def _find_meal_by_name(self, meal_name):
+        return next((m for m in self.menu if m.name == meal_name), None)
+
 
     def register_client(self, client_phone_number: str):
         client = self._find_client_by_number(client_phone_number)
@@ -23,6 +30,51 @@ class FoodOrdersApp:
         for meal in meals:
             if isinstance(meal, Meal) and type(meal).__name__ in self.VALID_MEALS:
                 self.menu.append(meal)
+
+    def show_menu(self):
+        if len(self.menu) < self.MIN_MEALS:
+            raise Exception('The menu is not ready!')
+        result = []
+        for meal in self.menu:
+            result.append(meal.details())
+
+        return '\n'.join(result)
+
+
+    def add_meals_to_shopping_cart(self, client_phone_number: str, **meal_names_and_quantities):
+        if len(self.menu) < self.MIN_MEALS:
+            raise Exception('The menu is not ready!')
+
+        client = self._find_client_by_number(client_phone_number)
+        if client is None:
+            self.register_client(client_phone_number)
+            client = self._find_client_by_number(client_phone_number)
+
+        ordered_meals = dict()
+        for ordered_meal, quantity in meal_names_and_quantities.items():
+            meal = self._find_meal_by_name(ordered_meal)
+            if meal is None:
+                raise Exception(f'{ordered_meal} is not on the menu!')
+            if meal.quantity < quantity:
+                raise Exception(f'Not enough quantity of {type(meal).__name__}: {meal.name}!')
+            ordered_meals[ordered_meal] = meal
+
+
+        for meal_name, quantity in meal_names_and_quantities.items():
+            meal = ordered_meals[meal_name]
+            meal.quantity -= quantity
+            if meal.quantity == 0:
+                self.menu.remove(meal)
+            ordered_meal = type(meal)(meal_name, meal.price, quantity)
+            client.shopping_cart.append(ordered_meal)
+            client.bill += ordered_meal.price * quantity
+
+        meal_names = ', '.join(m.name for m in client.shopping_cart)
+        return f'Client {client.phone_number} successfully ordered {meal_names} for {client.bill:.2f}lv.'
+
+
+
+
 
 
 
